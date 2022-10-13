d7114f9a5dc8138eeafbff4610ead6fff4cf3ac3
Jorge Eiras <j.eiras2020@gmail.com>
2022-10-13 05:19:54 -0300

    version02

--- Bucles/js/script.js
+++ Bucles/js/script.js
@@ -17,3 +17,50 @@ for (let i = 0; i < 4; i++) {
             console.error('En la iteracion: ' + (i += 1) + ' ' + 'esto es un mensaje de error');
     }
 }
+
+//Combinando ford con switch e if
+for(let i; i < 6; i++){
+    switch (i) {
+        case 0:
+            {
+                console.log('Estamos en el idice '+i);
+            }
+            break;
+        case 1:
+            {
+                let numero = "indefinida";
+                i % 0 ? numero == "par": numero== "inpar";
+                console.log('Estamos en el idice ' + (i+1)+ ' '+ 'aloja a un numero '+ numero);
+            }
+            break;
+        case 2:
+            {
+                let numero = "indefinida";
+                i % 0 ? numero == "par" : numero == "inpar";
+                console.log('Estamos en el idice ' + (i + 1) + ' ' + 'aloja a un numero ' + numero);
+            }
+            break;
+        case 3:
+            {
+                let numero = "indefinida";
+                i % 0 ? numero == "par" : numero == "inpar";
+                console.log('Estamos en el idice ' + (i + 1) + ' ' + 'aloja a un numero ' + numero);
+            }
+            break;
+        case 4:
+            {
+                let numero = "indefinida";
+                i % 0 ? numero == "par" : numero == "inpar";
+                console.log('Estamos en el idice ' + (i + 1) + ' ' + 'aloja a un numero ' + numero);
+            }
+            break;
+        case 5:
+            {
+                let numero = "indefinida";
+                i % 0 ? numero == "par" : numero == "inpar";
+                console.log('Estamos en el idice ' + (i + 1) + ' ' + 'aloja a un numero ' + numero);
+            }
+            break;
+    
+    }
+}
